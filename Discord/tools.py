ec2c8570b3c8d6b9ad5e9c5f0618bc7d758a9afc
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-04-01 10:17:45 -0300

    Add tools.py

---
+++ Discord/tools.py
@@ -0,0 +1,22 @@
+import re
+
+class Tools:
+    def __init__(self):
+        self.client_id = 'BOT CLIENT ID'
+        self.owner_id = 'BOT OWNER ID'
+        self.bot_token = 'BOT TOKEN'
+        self.trash = ['\r', '\n', '\t', '\0', '\'', '\"', '\\', '&', '|', '#', '$', '%', '<', '>', '(', ')', '{', '}', ';', '*', '[', ']']
+        self.email_pattern = "([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
+
+    def sanitize(self, message):
+        tmp = message
+        for c in self.trash:
+            if c in message:
+                tmp.replace(c, '')
+        return tmp
+
+    def check_string(self, string):
+        if re.match(self.email_pattern, string):
+            return True
+        else:
+            return False
