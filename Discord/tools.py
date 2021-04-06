c5975c5942283da0edf6bb16d7ddb5b0c4ebd405
rf-peixoto <50427765+rf-peixoto@users.noreply.github.com>
2021-04-06 12:08:14 -0300

    Update tools.py

--- Discord/tools.py
+++ Discord/tools.py
@@ -5,7 +5,7 @@ class Tools:
         self.client_id = 'BOT CLIENT ID'
         self.owner_id = 'BOT OWNER ID'
         self.bot_token = 'BOT TOKEN'
-        self.trash = ['\r', '\n', '\t', '\0', "'", '"', '\\', '&', '|', '#', '$', '%', '<', '>', '(', ')', '{', '}', ';', '*', '[', ']', '--', ' ']
+        self.trash = ['\r', '\n', '\t', '\0', "'", '"', '\\', '/', '&', '|', '#', '$', '%', '<', '>', '(', ')', '{', '}', ';', '*', '[', ']', '--', ' ']
         self.email_pattern = "([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)"
 
     def sanitize(self, message, counter=3):
